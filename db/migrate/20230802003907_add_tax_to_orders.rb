0d71e2cdb25da4329536e6ee88e98b93e9fd440b
ggimenez0115 <ggimenez@academic.rrc.ca>
2023-08-09 15:07:37 -0500

    final work

---
+++ db/migrate/20230802003907_add_tax_to_orders.rb
@@ -0,0 +1,7 @@
+class AddTaxToOrders < ActiveRecord::Migration[7.0]
+  def change
+    add_column :orders, :gst, :decimal
+    add_column :orders, :pst, :decimal
+    add_column :orders, :hst, :decimal
+  end
+end
---
+++ db/migrate/20230802213536_add_address_details_to_users.rb
@@ -0,0 +1,6 @@
+class AddAddressDetailsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :address, :string
+    add_reference :users, :province, null: false, foreign_key: true
+  end
+end
